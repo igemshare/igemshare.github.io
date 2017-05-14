6646b349f3a661355e8c920a9353ed3270ae1909
huyp <huyp@shanghaitech.edu.cn>
2017-05-15 01:40:07 +0800

    范围

---
+++ 执行.bat
@@ -0,0 +1,10 @@
+cd c:
+cd Users
+cd ºúÉÐÖ¾
+cd desktop
+cd git
+cd 5
+git add hello.png consoles.png rviz.png hello.py
+git commit -m"123"
+git remote add origin metastasis@gradebot.org:user/huyp/1/5
+git push origin master
