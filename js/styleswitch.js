474b9abf2ff0a70a05865fe7a21f411ef5589515
huyp <huyp@shanghaitech.edu.cn>
2017-08-16 13:15:26 +0800

    no message

--- js/styleswitch.js
+++ js/styleswitch.js
@@ -402,7 +402,7 @@ $(document).ready(function($){
 
     function changeLayout(layout) {
     $.cookie(cookieName, layout);
-    $('head link[data-name=skins]').attr('href', 'Template:Shanghaitech/css/skins/' + layout + '?action=raw&ctype=text/css');
+    $('head link[data-name=skins]').attr('href', 'http://2017.igem.org/Template:Shanghaitech/css/skins/' + layout + '?action=raw&ctype=text/css');
     }
 
     if( $.cookie(cookieName)) {
